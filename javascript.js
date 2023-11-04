fcf9e697842d07bf3ddec50ec5f9ede1147859d1
colin rice <100053207+katrice96@users.noreply.github.com>
2023-11-03 22:25:52 -0400

    changed load order
    changed js load order to allow for code to run properly

--- javascript.js
+++ javascript.js
@@ -1,9 +1,8 @@
-const container = document.getElementById('gridContainer');
+const rows = 16// make rows 
+const columns = 16 // rows are made and then columns are 
+const container = document.getElementById('gridContainer');//looks for grid id 
 
-const rows = 16
-const columns = 16 
-
-for ( let i = 0; i < rows; i++) {
+for ( let i = 0; i < rows; i++) { // fills 
     const row = document.createElement('div');
     row.classList.add('row');
 
@@ -15,4 +14,16 @@ for ( let i = 0; i < rows; i++) {
 
     container.appendChild(row);
 }
+
+const cells = document.querySelectorAll('.cell')
+
+cells.forEach(function(cell) {
+    console.log(cell);
+    cell.addEventListener('mouseover', function(){
+        if (!cell.classList.contains('hovered')) {
+            cell.classList.add('hovered');
+        }
+          console.log('mouse over cell')  
+    });
+});
 
